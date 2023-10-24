f9b9552fd9da9416554f194cafd38230649b1a55
oumniasabri <oumniasabri>
2023-10-24 15:40:28 +0100

    databases

---
+++ database/migrations/2023_10_24_141858_create_utilisateur_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUtilisateurTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilisateur', function (Blueprint $table) {
+            $table->bigIncrements('id_utilisateur');
+            $table->string('nom',40);
+            $table->string('email',50);
+            $table->string('password',1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilisateur');
+    }
+}
---
+++ database/migrations/2023_10_24_143748_create_produit_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProduitTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produit', function (Blueprint $table) {
+            $table->bigIncrements('id_produit');
+            $table->string('nom',40);
+            $table->float('prix');
+            $table->unsignedBigInteger('id_utilisateur');
+            $table->foreign('id_utilisateur')->references('id_utilisateur')->on('utilisateur');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produit');
+    }
+}
